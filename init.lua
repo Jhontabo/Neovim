d770020e43e1cc1b64b45c11a79957b47a9e6104
Jhontabo <jhonse.tajumbina@gmail.com>
2025-04-29 13:29:22 -0500

    first commit

---
+++ init.lua
@@ -0,0 +1,8 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+-- Asigna la tecla '-' para abrir oil.nvim
+-- Cargar los keymaps
+require("config.keymaps")
+
+vim.g.catppuccin_flavour = "mocha" -- O "latte", "macchiato", "mocha"
+vim.cmd("colorscheme catppuccin")
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+-- Asignar Ctrl + A para seleccionar todo
+vim.api.nvim_set_keymap("n", "<C-a>", "ggVGy", { noremap = true, silent = true })
+
+-- Asignar Ctrl + D para cortar
+vim.api.nvim_set_keymap("n", "<C-d>", "ggVGd", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "-", ":Oil<CR>", { noremap = true, silent = true })
