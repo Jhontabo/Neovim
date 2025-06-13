c2f0d328a9d4f4c52e63d912c7838b4a1536be16
Jhontabo <jhonse.tajumbina@gmail.com>
2025-06-13 09:47:20 -0500

    add copilot pluggin

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- Asigna la tecla '-' para abrir oil.nvim
--- Cargar los keymaps
 require("config.keymaps")
-vim.g.catppuccin_flavour = "frappe" -- O "latte", "macchiato", "mocha"
+vim.g.catppuccin_flavour = "mocha" -- O "latte", "macchiato", "mocha"
 vim.cmd("colorscheme catppuccin")
